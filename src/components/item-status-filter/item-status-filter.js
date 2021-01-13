bcce2d8f1697f75761b780afce4836a8ab2e623b
hobosapiens <trudovoe.popov@gmail.com>
2021-01-13 16:18:34 +0300

    Добавление классовых компоненты

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
-    return (
-        <div className="item-status-filter col s6">
-            <button type="button"
-                    className="btn transparent active">All
-            </button>
-            <button type="button"
-                    className="btn transparent">Active
-            </button>
-            <button type="button"
-                    className="btn transparent">Done
-            </button>
-        </div>
-    )
-};
-
-export default ItemStatusFilter;
+export default  class ItemStatusFilter extends Component {
+    render() {
+        return (
+            <div className="item-status-filter col s6">
+                <button type="button"
+                        className="btn transparent active">All
+                </button>
+                <button type="button"
+                        className="btn transparent">Active
+                </button>
+                <button type="button"
+                        className="btn transparent">Done
+                </button>
+            </div>
+        )
+    }
+}
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({label, important = false}) => {
+export default class TodoListItem extends Component {
 
-    const style = {
-        color: important ? '#26a69a' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`)
     };
 
-    return (
-        <div className="todo-list-item">
-            <span className="todo-list-item-label"
-                  style={style}>{label}
-            </span>
-            <button type="button"
-                    className="btn transparent">
-                <i className="material-icons">flash_on</i>
-            </button>
-            <button type="button"
-                    className="btn red lighten-1">
-                <i className="material-icons">cancel</i>
-            </button>
-        </div>
-    )
-};
+    render() {
 
-export default TodoListItem;
+        const {label, important = false} = this.props;
+
+        const style = {
+            color: important ? '#26a69a' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+
+        return (
+            <div className="todo-list-item">
+                <span className="todo-list-item-label"
+                      style={style}
+                      onClick={ this.onLabelClick }>{label}
+                </span>
+                <button type="button"
+                        className="btn transparent">
+                    <i className="material-icons">flash_on</i>
+                </button>
+                <button type="button"
+                        className="btn red lighten-1">
+                    <i className="material-icons">cancel</i>
+                </button>
+            </div>
+        )
+    }
+}
